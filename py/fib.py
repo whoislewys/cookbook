f8eb583a94876057627d09904ea2857dd8b106b5
Luis Gomez <whoislewys@gmail.com>
2019-09-10 21:01:08 -0700

    init commit. fib with memoization

---
+++ py/fib.py
@@ -0,0 +1,29 @@
+class fibonacci():
+    def nextFib(self, n):
+        if n == 0:
+            return 0;
+        if n == 1:
+            return 1;
+        return self.nextFib(n - 1) + self.nextFib(n - 2)
+
+    def memoizedNextFib(self, n, cache):
+        if n == 0:
+            return 0;
+        if n == 1:
+            return 1;
+
+        if n not in cache:
+            leftRec = self.memoizedNextFib(n - 1, cache)
+            rightRec = self.memoizedNextFib(n - 2, cache)
+            cache[n] = leftRec + rightRec
+            return leftRec + rightRec
+        else:
+            return cache[n]
+
+
+
+if __name__ == '__main__':
+    fibonacci = fibonacci()
+
+    print(fibonacci.nextFib(4))
+    print(fibonacci.memoizedNextFib(4, {}))
